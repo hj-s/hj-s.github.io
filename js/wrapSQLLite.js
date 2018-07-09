1fe0011294b87c688a2b5cb80805d63f11cac71c
hj-s <spam@hj-s.ru>
2018-07-09 17:01:24 +0500

    add time check
    for perfomance

--- js/wrapSQLLite.js
+++ js/wrapSQLLite.js
@@ -1,5 +1,6 @@
 const isDefined = (check) => (check !== undefined);
 const isFeature = (check, property) => ( isDefined(check) ? check.hasOwnProperty(property) : false);
+const log = () => console.log(this);
 
 const dhcomma = `"`;
 const hcomma = `'`;
@@ -19,7 +20,9 @@ var scomma, endLine, newLine;
 // 		return tag + this + tag
 // 	};
 // }
-(!String.prototype.WrapTag) ? String.prototype.WrapTag = function(tag){ return tag + this + tag;} : console.log(`WrapTag is used`);
+(!String.prototype.WrapTag) ? 
+	String.prototype.WrapTag = function(tag) { return tag + this + tag } 
+	: console.log(`WrapTag is used`);
 
 //add single comma to string 
 // if (!String.prototype.InComma){String.prototype.InComma = function(){
@@ -27,20 +30,26 @@ var scomma, endLine, newLine;
 // 				.WrapTag(hcomma)
 // 	};
 // }
-(!String.prototype.InComma) ? String.prototype.InComma = function(){ return this.replace(new RegExp(scomma,`g`), scomma + scomma).WrapTag(scomma)} : console.log(`InComma is used`);
+(!String.prototype.InComma) ? 
+	String.prototype.InComma = function(){ return this.replace(new RegExp(scomma,`g`), scomma + scomma).WrapTag(scomma) } 
+	: console.log(`InComma is used`);
 
 // if (!String.prototype.surrondLite){String.prototype.surrondLite = function(){
 // 		return this.replace(new RegExp(line,`g`), newLine )
 // 	};
 // }
-(!String.prototype.surrondLite) ? String.prototype.surrondLite = function(){ return this.replace(new RegExp(line,`g`), newLine )} : console.log(`surrondLite is used`);
+(!String.prototype.surrondLite) ? 
+	String.prototype.surrondLite = function(){ return this.replace(new RegExp(line,`g`), newLine) } : 
+	console.log(`surrondLite is used`);
 
 // wrap keyword
 // if (!String.prototype.wrapKeywordLite){String.prototype.wrapKeywordLite = function(keyword, wrapper){
 // 		return this.replace(new RegExp(keyword,`g`), wrapper+keyword)
 // 	}
 // }
-(!String.prototype.wrapKeywordLite) ? String.prototype.wrapKeywordLite = function(keyword, wrapper){ return this.replace(new RegExp(keyword,`g`), wrapper+keyword)} : console.log(`wrapKeywordLite is used`);
+(!String.prototype.wrapKeywordLite) ? 
+	String.prototype.wrapKeywordLite = function(keyword, wrapper){ return this.replace(new RegExp(keyword,`g`), wrapper + keyword) } 
+	: console.log(`wrapKeywordLite is used`);
 
 //wrap array of keywords
 // if (!String.prototype.handleArrayLite){String.prototype.handleArrayLite = function(keywords, wrapper){
@@ -51,13 +60,15 @@ var scomma, endLine, newLine;
 // 		return strSQL
 // 	};
 // }
-(!String.prototype.handleArrayLite) ? String.prototype.handleArrayLite = function(keywords, wrapper){
-	var strSQL = this;
-	for (keyword in keywords){
-		strSQL = strSQL.wrapKeywordLite(keywords[keyword], wrapper);
-	}
-	return strSQL
-} : console.log(`wrapKeywordLite is used`);
+(!String.prototype.handleArrayLite) ? 
+	String.prototype.handleArrayLite = function(keywords, wrapper){
+		var strSQL = this
+		for (keyword in keywords){
+			strSQL = strSQL.wrapKeywordLite(keywords[keyword], wrapper)
+		}
+		return strSQL
+	} 
+	: console.log(`wrapKeywordLite is used`);
 
 //wrap lines
 // if (!String.prototype.handleLines){String.prototype.handleLines = function(){
@@ -65,7 +76,9 @@ var scomma, endLine, newLine;
 // 				.handleArrayLite(keyrowdsMany, newLine + tab)
 // 	};
 // }
-(!String.prototype.handleLines) ? String.prototype.handleLines = function(){ return this.handleArrayLite(keywordsSingle,  newLine).handleArrayLite(keyrowdsMany, newLine + tab)} : console.log(`handleLines is used`);
+(!String.prototype.handleLines) ? 
+	String.prototype.handleLines = function(){ return this.handleArrayLite(keywordsSingle,  newLine).handleArrayLite(keyrowdsMany, newLine + tab) } 
+	: console.log(`handleLines is used`);
 
 //wrap SQL request
 // if (!String.prototype.handleSQL){String.prototype.handleSQL = function(){
@@ -74,50 +87,61 @@ var scomma, endLine, newLine;
 // 				.handleLines()
 // 	};
 // }
-(!String.prototype.handleSQL) ? String.prototype.handleSQL = function(){ return this.replace(/\n|\t/g,``).replace(/ , |, | ,/g, `,`).handleLines()} : console.log(`handleSQL is used`);
+(!String.prototype.handleSQL) ? 
+	String.prototype.handleSQL = function(){ return this.replace(/\n|\t/g,``).replace(/ , |, | ,/g, `,`).handleLines() } 
+	: console.log(`handleSQL is used`);
 
 //surround lines like `\n` -> ` ' + \\ \n'`
 // if (!String.prototype.surroundLinesWith){String.prototype.surroundLinesWith = function(){
 // 		return this.surroundWith(line, endLine, hcomma)
 // 	};
 //}
-(!String.prototype.surroundLinesWith) ? String.prototype.surroundLinesWith = function(){ return this.surroundWith(line, endLine, scomma)} : console.log(`surroundLinesWith is used`);
+(!String.prototype.surroundLinesWith) ? 
+	String.prototype.surroundLinesWith = function(){ return this.surroundWith(line, endLine, scomma) } 
+	: console.log(`surroundLinesWith is used`);
 
 //init wrapper args
-(!String.prototype.initWrap) ? String.prototype.initWrap = function(args = undefined){
-	//check args
-	let backslash = false;
-	if (isDefined(args) && isFeature(args, `backslash`)) {
-		backslash = args.backslash;
-	}
+(!String.prototype.initWrap) ? 
+	String.prototype.initWrap = function(args = undefined){
+		//check args
+		let backslash = false
+		if (isDefined(args) && isFeature(args, `backslash`)) {
+			backslash = args.backslash
+		}
 
-	//init 
-	scomma = hcomma;
-	endLine = space + scomma + space + plus + (backslash ? space + bslash : nt);
-	newLine = endLine + line + scomma;
-	return this;
-} : console.log(`initWrap is used`);
+		//init 
+		scomma = hcomma
+		endLine = space + scomma + space + plus + (backslash ? space + bslash : nt)
+		newLine = endLine + line + scomma
+		return this
+	} 
+	: console.log(`initWrap is used`);
 
 //main function wrap
-(!String.prototype.wrap) ? String.prototype.wrap = function(action, args = undefined){
-	console.log(`source: ` + line + this + line);	
-	var str = ``;
-	switch (action) {
-		case `wrap`: 
-			str = this.initWrap(args).trim()
-					.InComma()
-					.handleSQL();
-			break;
-		case `surround`:
-			str = this.initWrap(args).trim()
-					.InComma()
-					.surrondLite();
-			break;
-		default:
-			str = `Invalid action`;
-			console.log(str);
-			break;
-	}
-	console.log(`result: ` + line + str);
-	return str
-} : console.log(`wrap is used`);
+(!String.prototype.wrap) ? 
+	String.prototype.wrap = function(action, args = undefined){
+		console.log(`source: ` + line + this + line)
+		var str = ``
+		var time = isDefined(performance) ? performance.now() : 0
+		switch (action) {
+			case `wrap`: 
+				str = this.initWrap(args).trim()
+						.InComma()
+						.handleSQL()
+				break
+			case `surround`:
+				str = this.initWrap(args).trim()
+						.InComma()
+						.surrondLite()
+				break
+			default:
+				str = `Invalid action`
+				console.log(str)
+				break
+		}
+		time = isDefined(performance) ? performance.now() - time : 0
+		console.log(`result: ` + line + str)
+		console.log(`time: ` + time)
+		return str
+	} 
+	: console.log(`wrap is used`);
